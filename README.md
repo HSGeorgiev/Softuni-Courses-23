76810462a8416e3b81a5814738b6162b21b7b689
HSGeorgiev <147096400+HSGeorgiev@users.noreply.github.com>
2023-10-19 09:16:18 +0300

    A js file addes and Readme file modified

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Softuni-Courses-23
-Courses from the time I've get fun @Softuni
+## Courses from the time I've get fun @Softuni
 1. Test 1
 1. Test 2
 1. Test 3
+1. Test 4
---
+++ SomeScript.js
@@ -0,0 +1,5 @@
+//Some JS file here for some functionality..
+function SomeFunctionality (input){
+    //The actual function here...
+    let a = 'Some text to be printed.';
+}
