4ea94a6613e4390f48cedce12fa2935f5af5178f
HSGeorgiev <147096400+HSGeorgiev@users.noreply.github.com>
2023-10-19 19:30:21 +0300

    Some adds in the C# code

--- modul1.cs
+++ modul1.cs
@@ -1,1 +1,45 @@
 //Some C# code here
+using System;
+
+namespace _06BarcodeGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string startCode = Console.ReadLine();
+            string endCode = Console.ReadLine();
+
+            int startNumber = int.Parse(startCode);
+            int endNumber = int.Parse(endCode);
+
+
+
+            for (int i = startNumber; i < endNumber; i++)
+            {
+                bool isLegit = true;
+
+                string iCode = i.ToString();
+                //Console.WriteLine(iCode);
+                for (int j = 0; j < iCode.Length; j++)
+                {
+                    // Console.WriteLine(j);
+                    if (iCode[j] % 2 == 0)
+                    {
+                        isLegit = false;
+                    }
+                }
+
+                if (isLegit)
+                {
+                    Console.Write($"{iCode} ");
+                }
+            }
+            // Console.WriteLine(i);
+
+
+
+        }
+    }
