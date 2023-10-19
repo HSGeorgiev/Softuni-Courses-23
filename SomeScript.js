48d48a996ff5d94a4d763c6a53a5f82039f8bbce
HSGeorgiev <147096400+HSGeorgiev@users.noreply.github.com>
2023-10-19 20:37:17 +0300

    Two new JSfunctions

--- SomeScript.js
+++ SomeScript.js
@@ -7,4 +7,19 @@ function SomeFunctionality (input){
     console.log(b);
 }
 
+function circleArea (input){
+    let radius = input;
+    let area = radius * radius * Math.PI;
+    return area;
+}
+
+function circleLength(input){
+    let radius = input;
+    let length = 2 * Math.PI * radius;
+    return length;
+}
+
+
 SomeFunctionality ("Now we can print all the array")
+console.log(circleArea (1));
+console.log(circleLength(1));
